99a9bee73271453a901daf8f79a655b4ace4a570
Matthew Justice <matthewajustice@gmail.com>
2021-02-17 17:18:25 -0500

    Model built, just need to put in queries

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,33 @@
-// scheme-model
+const db = require('../../data/db-config');
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  remove,
+};
+
+function find() {
+  return db('schemes');
+}
+
+function findById(id) {
+  return db('schemes').where({ id }).first();
+}
+
+function findSteps(schemeId) {
+  return db('steps as st')
+    .join('schemes as sc', 'st.scheme_id', 'sc.id')
+    .where({ scheme_id: schemeId });
+}
+
+async function add(newScheme) {
+  const [id] = await db('schemes').insert(newScheme);
+
+  return findById(id);
+}
+
+function remove(id) {
+  return db('schemes').where({ id }).del();
+}
